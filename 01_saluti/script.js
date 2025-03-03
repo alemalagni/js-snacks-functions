cf0058da013637f78de2b4c4f33aef8124adf83a
alemalagni <alemalagni@gmail.com>
2025-03-03 14:20:50 +0100

    feat: add JS snack 01

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluto(name) {
+    return `ciao ${name}`
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(saluto(userName))
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
