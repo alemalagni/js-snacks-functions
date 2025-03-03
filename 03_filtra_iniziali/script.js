24b3f8b0e4b17b985b5068cd2c01dc38fa3e0eb7
alemalagni <alemalagni@gmail.com>
2025-03-03 16:08:15 +0100

    feat: add JS snack 03

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function namesLetter(letter, names) {
+    const letterNames = [];
+    for ( let i = 0; i < names.length; i++ ) {
+        const a = names[i];
+        if ( a[0] === letter ){
+            letterNames.push(a);
+        }
+    }
 
+    return letterNames;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const letter = prompt("Inserire una lettera:");
+if ( letter.length > 1 ) {
+    console.log("Errore")
+} else {
+    console.log(namesLetter(letter, names));
+}
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
