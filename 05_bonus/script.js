8da5e7cc7db350aa1f89b20492be2ae9c523d6c1
alemalagni <alemalagni@gmail.com>
2025-03-03 16:31:34 +0100

    feat: add JS snack 05

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,23 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluto(name, hour) {
+    if ( hour <= 13 ) {
+        return "Buongiorno " + name;
+    } else if ( hour <= 17 ) {
+        return "Buon pomeriggio " + name;
+    } else if ( hour > 17 ) {
+        return "Buonasera " + name;
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let hour = parseInt(prompt("Inserire l'orario:"));
+if ( hour < 0 || hour > 23 ) {
+    console.log("Errore");
+} else {
+    console.log(saluto(name, hour));
+}
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
