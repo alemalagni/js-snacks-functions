6fc7acae20a7ed1c9c8bc1eb84ca990095b3e5f3
alemalagni <alemalagni@gmail.com>
2025-03-03 16:15:22 +0100

    feat: add JS snack 04

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,18 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function vocals(word) {
+    let vocalNumber = 0;
+    for ( let i = 0; i < word.length; i++ ) {
+        if ( word[i] === "a" || word[i] === "e" || word[i] === "i" || word[i] === "o" || word[i] === "u" ) {
+            vocalNumber += 1;
+        }
+    }
+    return vocalNumber;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(vocals(word))
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
